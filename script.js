f1160ffb704172783f9f294eafa739276375a3f0
Alyaesub <reynierpascal@yahoo.com>
2025-01-29 21:44:14 +0100

    add pacman and function for casegame

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("test");
 
 /*
-* Créer le plateau (dynamique)
+OK Créer le plateau (dynamique)
 * Créer notre pacman
 * Gérer ses déplacements (sans contrainte)
 * Contraintes de déplacement (pas dans les murs)
@@ -11,7 +11,6 @@ console.log("test");
     Gérer collision pacman et un fantome
     Gérer les power-pellet (un mode ou pacman peut manger un fantome)
 */
-const gameDiv = document.getElementById("game");
 
 const layout = [
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -53,12 +52,14 @@ const layout = [
 // 3 - power-pellet
 // 4 - empty
 
-creerPlateau();
+const gameDiv = document.getElementById("game");
 
 function creerPlateau() {
+	let cptCase = 0; // va servire a donner un chiffre unique a chaque case du plateau
 	//pour chacunne d'elle (les cases, les chiffres) avec le forEach
 	layout.forEach((caseLayout) => {
 		let casePlateau = document.createElement("div"); // on crée les div ici avec creatElement
+		casePlateau.dataset.numerocase = cptCase; // datasat nous permet de donne les data attribu et ça nous incrément bien chaqu'une des div donc 783
 		//le switch "en fonction" du case layout donc du chiffre
 		switch (caseLayout) {
 			//pour layout 0 (points)
@@ -70,7 +71,7 @@ function creerPlateau() {
 				casePlateau.classList.add("mur"); // on ajoute la classe "murs" a toute les div des cases 1
 				break;
 			case 2:
-				casePlateau.classList.add("ghost-lair");
+				casePlateau.classList.add("ghost-lair"); // et pareil pour chacune
 				break;
 			case 3:
 				casePlateau.classList.add("power-pellet");
@@ -81,5 +82,19 @@ function creerPlateau() {
 		}
 
 		gameDiv.appendChild(casePlateau); // on ajoute le let a tous les chil de gameDiv
+		cptCase++; //incrément le compteur pour les chiffres des cases
 	});
+	///// on crée le pacman et lui donne le point de spawn ////
+	// let caseGame = document.querySelector('[data-numerocase="489"]');
+	// caseGame.classList.add("pacman");
+	getCaseByIndex(489).classList.add("pacman");
 }
+
+//// crée une fonction qui permettra de return une case une case depuis l'index//////
+function getCaseByIndex(index) {
+	let caseGame = document.querySelector(`[data-numerocase="${index}"]`);
+	return caseGame;
+}
+
+//appelle la fonction pour créer le plateau
+creerPlateau();
